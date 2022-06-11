40a07746259e00bfdd561a3b360e2d7bf0e54f29
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-06-11 20:50:01 +0100

    Generate Reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_11_192921) do
+ActiveRecord::Schema[7.0].define(version: 20_220_611_194_739) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'reservations', force: :cascade do |t|
+    t.date 'start_date'
+    t.date 'end_date'
+    t.decimal 'cost'
+    t.string 'city'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id', null: false
+    t.bigint 'yacht_id', null: false
+    t.index ['user_id'], name: 'index_reservations_on_user_id'
+    t.index ['yacht_id'], name: 'index_reservations_on_yacht_id'
   end
 
-  create_table "yachts", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.decimal "price"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  create_table 'yachts', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.decimal 'price'
+    t.string 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  add_foreign_key 'reservations', 'users'
+  add_foreign_key 'reservations', 'yachts'
 end
